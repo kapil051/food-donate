28c58e7e8a2789a4572b0248f6c63c42867d51dc
Sreejita2023 <sensreejita67@gmail.com>
2024-07-02 15:22:25 +0530

    Add Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-orange-300">
+    <h1 className="text-3xl font-bold underline bg-red-500 ">
       Hello world!
     </h1>
   )
---
+++ frontend/src/pages/volunteer/AvailableFood.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AvailableFood() {
+  return (
+    <div>AvailableFood</div>
+  )
+}
+
+export default AvailableFood
