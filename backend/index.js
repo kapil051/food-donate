f3198919c6afeb87b4e86f0239ceacdaa728ffb8
Sreejita2023 <sensreejita67@gmail.com>
2024-07-16 13:41:29 +0530

    donar form

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,6 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.js"
 import foodRouter from "./routes/food.js"
-
-
-// const corsOptions={
-//     origin:"http://localhost:5173",
-//     methods:'GET,POST,PUT,DELETE,PATCH,HEAD',
-//     credentials:true,
-// }
-
 import dotenv from 'dotenv';
 
 
@@ -19,7 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT || 4000
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors({
+    origin:"http://localhost:5173",
+	credentials: true,
+}));
 app.use("/user", userRouter);
 app.use("/food", foodRouter);
 
